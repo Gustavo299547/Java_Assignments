6206f9f23618bd3d1d69867ae9f9c2e4d0b73fef
Gustav <aywsf0699@gmail.com>
2026-08-18 19:39:32 +0300

    session02

---
+++ src/Session02/AddTwoNumbers.java
@@ -0,0 +1,31 @@
+package Session02;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+//        boolean response;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        do {
+//            // What we need
+//            System.out.println("Please Enter First Number");
+//            int num1 = scanner.nextInt();
+//
+//            System.out.println("Please Enter Second Number");
+//            int num2 = scanner.nextInt();
+//
+//            // Calculation
+//            int sum = num1 + num2;
+//
+//            System.out.println("The sum is " + sum);
+//
+//            System.out.println("Do you want to continue? 1-true 2-false");
+//
+//            response = scanner.nextBoolean();
+//
+//        } while (response);
+//
+//        scanner.close();
+    }
+}
